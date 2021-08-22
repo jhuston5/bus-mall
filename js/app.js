164a35ed37ce4523ac9c7accb90d06e7d062be4b
Joshua Huston <joshua.david.huston@gmail.com>
2021-08-21 21:54:40 -0400

    Attempted to add local storage

--- js/app.js
+++ js/app.js
@@ -15,19 +15,70 @@ let image3 = document.querySelector('section img:nth-child(3)');
 // Set click counters
 let clicks = 0;
 let clickAllowed = 25;
-let productArray = [];
+let randomNumbersArray = [];
 let numberOfUniqueIndexes = 6;
 
 
 // Constructor function creating a new product
 // Takes parameters that create the file name
-function Product(name, fileExtension = 'jpg') {
+function Product(name, fileExtension = 'jpg', views = 0, clicks = 0) {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
-  this.views = 0;
-  this.clicks = 0;
+  this.views = views;
+  this.clicks = clicks;
+  this.fileExtension = fileExtension;
   allProducts.push(this);
 };
+
+
+function makeAProduct(name, fileExtension, views, clicks) {
+  let productObj = new Product(name, fileExtension, views, clicks);
+  // renderProducts();
+}
+
+function storeAProduct(){
+  let stringifiedProducts = JSON.stringify(allProducts);
+  localStorage.setItem('products', stringifiedProducts);
+}
+
+
+function getProducts() {
+  console.log('I fired');
+  let potentialProductPicks = localStorage.getItem('products');
+  if (potentialProductPicks) {
+    let parsedProducts = JSON.parse(potentialProductPicks);
+    for (let order of parsedProducts) {
+      let name = order.name;
+      let fileExtension = order.fileExtension;
+      let views = order.views;
+      let clicks = order.clicks;
+      makeAProduct(name, fileExtension, views, clicks)
+      console.log(allProducts);
+    } 
+  } else {
+    new Product('sweep', 'png');
+    new Product('bag');
+    new Product('banana');
+    new Product('bathroom');
+    new Product('boots');
+    new Product('breakfast');
+    new Product('bubblegum');
+    new Product('chair');
+    new Product('cthulhu');
+    new Product('dragon');
+    new Product('pen');
+    new Product('pet-sweep');
+    new Product('shark');
+    new Product('tauntaun');
+    new Product('unicorn');
+    new Product('water-can');
+    new Product('wine-glass');
+    console.log(allProducts);
+  }
+
+  renderProducts();
+}
+
 
 // Function to create a random number that will be used to select the a product image from an array index
 function selectRandomProduct() {
@@ -35,16 +86,17 @@ function selectRandomProduct() {
 }
 
 function renderProducts() {
-  while (productArray.length < numberOfUniqueIndexes) {
-  let randomProduct = selectRandomProduct();
-  if (!productArray.includes(randomProduct)) {
-    productArray.push(randomProduct);
+  console.log(randomNumbersArray)
+  while (randomNumbersArray.length < numberOfUniqueIndexes) {
+    let randomProduct = selectRandomProduct();
+    if (!randomNumbersArray.includes(randomProduct)) {
+      randomNumbersArray.push(randomProduct);
+    }
   }
-}
 
-let product1 = productArray.shift();
-let product2 = productArray.shift();
-let product3 = productArray.shift();
+  let product1 = randomNumbersArray.shift();
+  let product2 = randomNumbersArray.shift();
+  let product3 = randomNumbersArray.shift();
 
   image1.src = allProducts[product1].src;
   image2.src = allProducts[product2].src;
@@ -70,15 +122,14 @@ function handleProductClick(e) {
   clicks++;
   let clickProduct = e.target.alt;
   for (let i = 0; i < allProducts.length; i++) {
-    console.log(clickProduct);
-    console.log(allProducts[i].name);
     if (clickProduct === allProducts[i].name) {
       allProducts[i].clicks++;
       break;
     }
   }
-  renderProducts();
+  // renderProducts();
   if(clicks === clickAllowed) {
+    storeAProduct();
     myContainer.removeEventListener('click', handleProductClick);
     renderChart();
   }
@@ -86,74 +137,53 @@ function handleProductClick(e) {
 
 
 
-new Product('sweep', 'png');
-new Product('bag');
-new Product('banana');
-new Product('bathroom');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('shark');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('water-can');
-new Product('wine-glass');
+function renderChart() {
+  let productClicks = [];
+  let productViews = [];
+  let productNames = [];
+  for (let i = 0; i < allProducts.length; i++) {
+    productNames.push(allProducts[i].name);
+    productClicks.push(allProducts[i].clicks);
+    productViews.push(allProducts[i].views);
+  }
 
-renderProducts();
 
+  let chartObject = {
+    type: 'bar',
+    data: {
+        labels: productNames,
+        datasets: [{
+            label: '# of Views',
+            data: productViews,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            hoverBorderWidth: 2,
+            borderWidth: 1
+        },
+        {
+          label: '# of Clicks',
+          data: productClicks,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor:  'rgba(255, 99, 132, 1)',
+          hoverBorderWidth: 2,
+          borderWidth: 1
+      }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+  };
+
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, chartObject);
+
+};
 
 
 myContainer.addEventListener('click', handleProductClick);
 
-
-function renderChart() {
-let productClicks = [];
-let productViews = [];
-let productNames = [];
-for (let i = 0; i < allProducts.length; i++) {
- productNames.push(allProducts[i].name);
- productClicks.push(allProducts[i].clicks);
- productViews.push(allProducts[i].views);
-}
-
-
-let chartObject = {
-  type: 'bar',
-  data: {
-      labels: productNames,
-      datasets: [{
-          label: '# of Views',
-          data: productViews,
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          hoverBorderWidth: 2,
-          borderWidth: 1
-      },
-      {
-        label: '# of Clicks',
-        data: productClicks,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor:  'rgba(255, 99, 132, 1)',
-        hoverBorderWidth: 2,
-        borderWidth: 1
-    }]
-  },
-  options: {
-      scales: {
-          y: {
-              beginAtZero: true
-          }
-      }
-  }
-};
-
-let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, chartObject);
-
-};
-
+getProducts();
